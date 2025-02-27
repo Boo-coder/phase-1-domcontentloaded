5db2bc4435577b027e28c22a1024cc023cf82b29
Boo-coder <Bouna112@yahoo.com>
2025-02-27 12:19:54 -0500

    LOADC

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", (e) => {
+    const updateText = document.getElementById("text")
+    updateText.innerHTML = "This is really cool!"
+})
